86550db5801889429fcf415e61214305d1a66401
Dominik Schelske (Domske) <190346505+DomskeFhain@users.noreply.github.com>
2025-04-16 18:54:02 +0200

    start

---
+++ hausaufgabe/src/TierCard.js
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function TierCard() {
+  return (
+    <div className="App">
+      <h2 className='App-Card'></h2>
+      </div>
+  )
+}
+
+export default TierCard;
---
+++ hausaufgabe/src/Titel.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function Titel() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Tierliste React</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Titel;
